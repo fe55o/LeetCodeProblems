783e26286cd896f147a192b395ca17c46616de4a
Mahmoud Tarek <49061606+fe55o@users.noreply.github.com>
2025-12-14 12:42:02 +0200

    Time: 3 ms (60%), Space: 58.3 MB (26.73%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.js
@@ -0,0 +1,13 @@
+var duplicateZeros = function(arr) {
+    const length = arr.length;
+
+    for (let i = 0; i < length; i++) {
+        if (arr[i] === 0) {
+            arr.splice(i, 0, 0);
+            i++;
+        }
+    }
+
+    arr.length = length;
+    return arr
+};
